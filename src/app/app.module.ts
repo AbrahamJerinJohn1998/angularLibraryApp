b34af5b4dc8f95fee634b02b982f4210c4b3f0f3
Abraham-Jerin-John <abrahamkalarickal98@gmail.com>
2022-11-24 20:19:27 +0530

    user Signup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { AdminLoginComponent } from './admin-login/admin-login.component';
+import { UserSignupComponent } from './user-signup/user-signup.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    AdminLoginComponent,
+    UserSignupComponent
   ],
   imports: [
     BrowserModule,
